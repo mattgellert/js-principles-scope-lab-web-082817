95c1ce00815f057874aa66daed5a0d22c545f4e2
mattgellert <mattgellert@gmail.com>
2017-10-10 09:58:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'matt'
+  leastFavoriteCustomer = 'bob'
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer;
+  let favoriteCustomer;
+}
